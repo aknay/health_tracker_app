6bc2f3abbe8110f63419ec40a569c59d959c67a7
aknay <aknay@outlook.com>
2022-06-20 20:27:33 +0800

    just improve tab ui

---
+++ lib/presentation/widgets/up_to_past_three_month_tab_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class UpToPastThreeMonthTabBar extends StatelessWidget {
+  const UpToPastThreeMonthTabBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.indigo,
+      indicator: BoxDecoration(
+          borderRadius: BorderRadius.circular(13), // Creates border
+          color: Colors.blue),
+      tabs: [
+        Tab(
+            child: Text(
+          AppLocalizations.of(context)!.past7Days,
+          textAlign: TextAlign.center,
+          style: const TextStyle(fontSize: 14, height: 1.5),
+        )),
+        Tab(
+          child: Text(AppLocalizations.of(context)!.oneMonth,
+              textAlign: TextAlign.center, style: const TextStyle(fontSize: 14, height: 1.5)),
+        ),
+        Tab(
+          child: Text(AppLocalizations.of(context)!.threeMonthsTabText,
+              textAlign: TextAlign.center, style: const TextStyle(fontSize: 14, height: 1.5)),
+        ),
+      ],
+    );
+  }
+}
